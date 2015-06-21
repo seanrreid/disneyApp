204ae652838981a5cb99efefe603b84c7c649014
Jamie Holding <me@jamie.holdings>
2015-06-21 13:04:02 +0100

    Add mobile resources

---
+++ mobile-config.js
@@ -0,0 +1,12 @@
+App.info({
+  name: 'Disneyland Paris Wait Times',
+  description: 'Simple app to show the Disneyland Paris wait times',
+  version: '0.0.1'
+});
+
+App.icons({
+  'android_ldpi': 'resources/icons/icon-ldpi.png',
+  'android_mdpi': 'resources/icons/icon-mdpi.png',
+  'android_hdpi': 'resources/icons/icon-hdpi.png',
+  'android_xhdpi': 'resources/icons/icon-xhdpi.png'
+});
